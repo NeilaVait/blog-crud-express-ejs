cae3e11c54ec64e55a67df1b43fbeb90a1ab03d4
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-05-31 10:44:34 +0300

    common head, nav, footer

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ app.get('/', function (req, res) {
 
 // about page
 app.get('/about', function (req, res) {
-  res.render('about');
+  res.render('about', {
+    title: 'About us',
+  });
 });
 
 // blog page
