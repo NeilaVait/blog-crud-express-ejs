aae2f5261d728b0479af15603833712f9a99b2ea
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-06-03 15:41:07 +0300

    single owner view works

--- src/routes/ownersRoutes.js
+++ src/routes/ownersRoutes.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
   // get all owners from db
 
   Owner.find()
+    .sort({ createdAt: -1 })
     .then((owners) => {
       res.render('owners/index', {
         title: 'Owners',
@@ -36,6 +37,22 @@ router.post('/new', (req, res) => {
       res.redirect('/owners?msg=Success');
     })
     .catch((err) => res.send('oops did not save', err));
+});
+
+// single owner route
+router.get('/single/:id', function (req, res) {
+  const ownerId = req.params.id;
+
+  Owner.findById(ownerId)
+    .then((owner) => {
+      res.render('owners/single', {
+        title: 'Single',
+        page: 'single_owner',
+        owner,
+      });
+    })
+    // redirect if not found
+    .catch((err) => console.error(err));
 });
 
 module.exports = router;
